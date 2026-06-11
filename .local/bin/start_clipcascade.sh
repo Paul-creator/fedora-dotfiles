2077d911ead8a65bfe96327c51e8c7bbf3c1185d
Paul-creator <Paul-creator@github.io>
2026-06-11 10:28:00 +0200

    added .local/bin and zshenv

---
+++ .local/bin/start_clipcascade.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Conda initialisieren
+source ~/Programs/miniconda3/etc/profile.d/conda.sh
+
+# Base environment aktivieren
+conda activate base
+
+# Programm starten
+python /home/paul/Programs/ClipCascade/main.py
---
+++ .zshenv
@@ -0,0 +1,6 @@
+. "$HOME/.cargo/env"
+
+xhost +local: > /dev/null 2>&1
+
+export PATH=$PATH:~/Programs/intelFPGA_lite/20.1/modelsim_ase/bin/
+
